cbbdf8455b1dcb4e1316ef1255961172924962a0
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-20 12:08:17 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <myMain />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myMain from './components/myMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myMain
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style/global";
+@import "./assets/style/utils";
+
 </style>
---
+++ src/components/myMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>CIAO</h1>
+</template>
+
+<script>
+export default {
+  name: "myMain"
+}
+</script>
+
+<style>
+
+</style>
