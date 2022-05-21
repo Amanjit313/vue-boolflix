6718ae77ee87cdb52a149d4606a75fdc8816dd3a
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-21 14:04:39 +0200

    Lista search film funzionante tramite il console log

---
+++ src/components/myHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>myHeader</h1>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: "myHeader",
+
+  data(){
+    return{
+      apiURL: "https://api.themoviedb.org/3/search/movie",
+      apiParams: {
+        api_key: "33253eee85c97808d758bc69d5359747",
+        language: "it-IT",
+        query: "abcd"
+      }
+    }
+  },
+
+  mounted(){
+    this.getAPI()
+  },
+
+  methods:{
+    getAPI(){
+      axios.get(this.apiURL, {
+        params: this.apiParams
+      })
+      .then(res =>{
+        console.log(res.data)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>CIAO</h1>
+  <h1>myMain</h1>
 </template>
 
 <script>
@@ -8,6 +8,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
