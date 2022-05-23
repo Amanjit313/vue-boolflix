f472db53f6828b71936241144b8a0c3defdfce06
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-23 17:50:35 +0200

    ridimensionato il poster

--- src/components/filmCard.vue
+++ src/components/filmCard.vue
@@ -1,7 +1,7 @@
 <template>
   
   <div>
-    <img :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" alt="">
+    <img :src="`https://image.tmdb.org/t/p/w300/${film.poster_path}`" alt="">
     <h1>Titolo Film: {{film.title || film.name}}</h1>
     <p>Titolo Originale: {{film.original_title || film.original_name}}</p>
     <p><lang-flag :iso="film.original_language" :squared="false"/></p>
