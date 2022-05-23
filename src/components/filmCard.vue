9c3d580c264624045a58890a3f2ec513e0c608fa
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-23 18:19:08 +0200

    Aggiunte le stelle dinamiche a seconda del voto medio di ogni film/serie tv

--- src/components/filmCard.vue
+++ src/components/filmCard.vue
@@ -2,10 +2,10 @@
   
   <div>
     <img :src="`https://image.tmdb.org/t/p/w300/${film.poster_path}`" alt="">
-    <h1>Titolo Film: {{film.title || film.name}}</h1>
-    <p>Titolo Originale: {{film.original_title || film.original_name}}</p>
+    <h1>{{film.title || film.name}}</h1>
+    <p>{{film.original_title || film.original_name}}</p>
     <p><lang-flag :iso="film.original_language" :squared="false"/></p>
-    <p>Voto: {{film.vote_average}}</p>
+    <star-rating :rating="film.vote_average/2" :read-only="true" :increment="0.1"></star-rating>
     <hr>
   </div>
 
@@ -13,11 +13,13 @@
 
 <script>
 import LangFlag from 'vue-lang-code-flags';
+import StarRating from 'vue-star-rating';
 
 export default {
   name: "myMain",
   components: {
-    LangFlag
+    LangFlag,
+    StarRating
   },
   props: {
     film : Object
