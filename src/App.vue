278f5817dc9d842bf34a33a098f52c1b2a0cd4df
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-23 19:24:21 +0200

    costumizzata la mia search-bar

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,13 @@ export default {
 @import "./assets/style/global";
 @import "./assets/style/utils";
 
+div{
+  background-color: #212121;
+}
+
 </style>
+
+/* PROMEMORIA DEGLI ERRORI */
+- Sistemare le bandiere non presenti nella cdn,
+- Sistemare il bug se ho già cliccato serie tv e cerco qualcosa mi mostra comunque prima e film e dopo ricliccato film e serie tv mi mostra le serie tv prima no,
+- Sistemare gli elementi che non hanno una immagine di default dandone una dinamicamente
--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="search-bar">
-      <input v-model.trim = "movieToSearch" placeholder="Cerca un film" type="text" @keyup.enter="$emit('startSearch', movieToSearch)">
-      <button type="button" @click ="$emit('startSearch', movieToSearch)">SEARCH</button>
 
-      <select v-model ="selector" @change ="$emit('selectorType', selector)">
-        <option value="film">Film</option>
-        <option value="serie tv">Serie TV</option>
-      </select>
+  <div class="search-bar-container">
+
+    <div class="logo">
+      <h1>BOOLFLIX</h1>
+    </div>
+
+    <div class="search-bar">
+      <div class="costum-border">
+        <input v-model.trim = "movieToSearch" placeholder="Cerca un film" type="text" @keyup.enter="$emit('startSearch', movieToSearch)">
+        <button type="button" @click ="$emit('startSearch', movieToSearch)">SEARCH</button>
+      </div>
+
+        <select v-model ="selector" @change ="$emit('selectorType', selector)">
+          <option value="film">Film</option>
+          <option value="serie tv">Serie TV</option>
+        </select>
+    </div>
+
   </div>
+
 </template>
 
 <script>
@@ -25,24 +37,67 @@ export default {
 
 <style lang="scss" scoped>
 
+.search-bar-container{
+  display: flex;
+  background-color: #212121;
+  padding: 20px;
+  justify-content: space-between;
+  border-bottom: 1px solid #E0E0E0;
+}
+
+h1{
+  color: red;
+}
+
 .search-bar{
   display: flex;
-  justify-content: center;
-  margin-top: 50px;
+  align-items: center;
 }
 
-input{
-  margin-right: 20px;
-  height: 2rem;
-}
-
-button{
-  padding: 5px;
+.costum-border{
+  padding: 10px;
+  border: 1px solid #E0E0E0;
   border-radius: 5px;
 }
 
+input{
+  margin-right: 5px;
+  height: 2rem;
+  border: none;
+  background-color: transparent;
+  border-bottom: 1px solid #E0E0E0;
+  color: rgb(255, 255, 255);
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid #2196F3;
+  }
+}
+
+button{
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #2196F3;
+  color: white;
+  border: #2196F3;
+
+  &:hover{
+    background-color: #0987ef;
+  }
+}
+
 select{
-  margin-left: 10px;
+  margin-left: 30px;
+  padding: 10px;
+  background-color: transparent;
+  color: lightgray;
+  border: #2196F3;
+  border-left: 1px solid #E0E0E0;
+  text-transform: uppercase;
+
+  &:hover{
+    color: white;
+  }
 }
 
 </style>
